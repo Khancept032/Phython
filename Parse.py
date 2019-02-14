0aaf6a4b8968d4ac62e8a186beb8d0dc81627bc0
chrichoq <47570913+chrichoq@users.noreply.github.com>
2019-02-14 12:28:59 -0600

    Update Parse.py

--- Parse.py
+++ Parse.py
@@ -1,1 +1,1 @@
-The master file of the programs will be placed in here once we have finished the programs for each question.
+# The master file of the programs will be placed in here once we have finished the programs for each question.
