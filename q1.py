02c947c82b8bb4fb659cb106413995d7bc174bcf
Abdul Khan <abdul_khan@tamu.edu>
2019-02-18 14:55:00 -0600

    Wrote code to count total requests

--- q1.py
+++ q1.py
@@ -1,1 +1,10 @@
-# File for question 1.
+# Total count of lines
+
+def file_len(fname):
+    with open(fname) as l:
+        for i, x in enumerate(l):
+            pass
+    return i + 1        
+
+print("Total Request:") 
+print(file_len("local_copy.log")) 
