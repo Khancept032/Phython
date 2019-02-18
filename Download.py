bf2ae3d26b26c7abedbc751a7e6b9e6ed3b7dc62
Abdul Khan <abdul_khan@tamu.edu>
2019-02-18 14:23:56 -0600

    Fixed the download issue

--- Download.py
+++ Download.py
@@ -1,12 +1,13 @@
 # This will download the log files to your local machine
 
+print('Starting file download...')
+
 from urllib.request import urlretrieve
 
-URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access-log'
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
 
+urlretrieve (URL_PATH, LOCAL_FILE )
 
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True))
 
-
-print('Starting file download...')
+print('File has downloaded')
