7750832e41f8325b24d6151e492737cb34ae76fa
chrichoq <chrichoq@gmail.com>
2019-02-19 07:32:06 -0600

    Updated single files to include log download

--- q2.py
+++ q2.py
@@ -1,8 +1,14 @@
+# Download log file
+print('Starting file download...')
+
+from urllib.request import urlretrieve
+
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+urlretrieve (URL_PATH, LOCAL_FILE )
+
+print('File has downloaded.')
+
 # File for question 2 solutions.
 # How many requests were made on each day? per week? per month?
-
-# Requests per day
-
-# Requests per week
-
-# Requests per month
--- q4.py
+++ q4.py
@@ -1,3 +1,15 @@
+# Download log file
+print('Starting file download...')
+
+from urllib.request import urlretrieve
+
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+urlretrieve (URL_PATH, LOCAL_FILE )
+
+print('File has downloaded.')
+
 # File for question 4 solutions.
 
 # What percentage of the requests were redirected elsewhere (any 3xx codes)?
