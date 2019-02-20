a8cfb5145a5ed61bcaecd36c10c5adf0be7fef92
Abdul Khan <abdul_khan@tamu.edu>
2019-02-20 09:44:01 -0600

    stripping files, changed code

--- q5.py
+++ q5.py
@@ -1,50 +1,53 @@
 # Files for question 5.
 
 #Below is incomplete and does not work yet.
+      
+FILE_NAME = 'local_copy.log'  
+f = open('local_copy.log')
 
-# def most():
-   # import operator
-   # x = open("locl_copy.txt", "r")
-   # line = []
-   # countReq = {}
-   # filelist = []
+gifcount = 0
+tiffcount = 0
+execount = 0
+txtcount = 0
+xbmcount = 0
+pscount = 0
+htmlcount = 0
 
-   # count = 0
-   # for line in x:
-    #    count+=1
-     #   line.append(line)
-   # for i in range(len(line)):
-    #    listSplit = line[int(i)].split()
-     #   try: reqFile = listSplit[6]
-      #  except: pass
-       # filelist.append(reqFile)
-   # for key in filelist:
-    #    if key not in countReq:
-     #       countReq[key] = 1    
-      #  else:
-       #      if key in countReq:
-        #        countReq[key] += 1
+lines = []
 
-#testing a method to count the most requested file
-#currently this below doesnt do exactly what we need it to, it uses dictionary. 
 
-print('Retrieving Most Requested File...')
-fname = 'local_copy.log'
-if len(fname) < 1 : fname = 'local_copy.log'
-hand = open(fname)
+for line in f:
+       line = line.strip()
+       lines.append(line)
+# stripping for files
+for a in range (len(lines)):
+       try:
+              results = lines[a].split()[6]
+       except:
+              continue
+       if results.endswith('gif'):
+              gifcount += 1
+              continue
+       if results.endswith('tiff'):
+              tiffcount += 1
+              continue
+       if results.endswith('exe'):
+              execount += 1
+              continue
+       if results.endswith('txt'):
+              txtcount += 1
+              continue 
+       if results.endswith('xbm'):
+              xbmcount += 1
+              continue
+       if results.endswith('ps'):
+              pscount += 1
+              continue
+       if results.endswith('html'):
+              htmlcount += 1
+              continue
+       else:
+              continue     
 
-d = dict()
-for lin in hand:
-    lin = lin.rstrip()
-    wds = lin.split()
-    for w in wds:
-       d[w] = d.get(w,0) + 1 
-  
-largest = -1
-theword = None
-for k,v in d.items() :
-        if v in d :
-            largest = v
-            theword = k
-print('Most Requested:',theword,largest)            
-       
+         
+
