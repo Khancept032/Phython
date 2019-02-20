201a9453f4a34ceea14145ce0188fba00242666c
Abdul Khan <abdul_khan@tamu.edu>
2019-02-20 10:29:59 -0600

    finished most frequent request

--- q5.py
+++ q5.py
@@ -1,10 +1,10 @@
-# Files for question 5.
+# Code for most requested file
 
-#Below is incomplete and does not work yet.
       
 FILE_NAME = 'local_copy.log'  
 f = open('local_copy.log')
 
+# each file is set to zero so it can be used to count after stripping
 gifcount = 0
 tiffcount = 0
 execount = 0
@@ -13,12 +13,12 @@ xbmcount = 0
 pscount = 0
 htmlcount = 0
 
+
 lines = []
-
-
 for line in f:
        line = line.strip()
        lines.append(line)
+
 # stripping for files
 for a in range (len(lines)):
        try:
@@ -49,5 +49,30 @@ for a in range (len(lines)):
        else:
               continue     
 
-         
+
+# below uses max to count the most frequent file
+files = (gifcount, tiffcount, execount, txtcount, xbmcount, pscount, htmlcount)
+
+if gifcount == max(files):
+    print('gif is most requested file:', max(files))
+
+elif tiffcount == max(files):
+    print('tiff is most requested file:', max(files))
+
+elif execount == max(files):
+    print('exe is most requested file:', max(files))
+
+elif txtcount == max(files):
+    print('txt is most requested file:', max(files))
+
+elif xbmcount == max(files):
+    print('xbm is most requested file:', max(files))
+
+elif pscount == max(files):
+    print('ps is most requested file:', max(files))
+
+elif htmlcount == max(files):
+    print('html is most requested file:', max(files))
+
+print()
 
