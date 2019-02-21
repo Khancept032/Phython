cf8cd0567f0e09bd732751be581d370dad7e571c
chrichoq <47570913+chrichoq@users.noreply.github.com>
2019-02-21 11:01:57 -0600

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,14 +1,32 @@
 # Download log file
-print('Starting file download...')
+#print('Starting file download...')
 
-from urllib.request import urlretrieve
+#from urllib.request import urlretrieve
 
-URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-LOCAL_FILE = 'local_copy.log'
+#URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+#LOCAL_FILE = 'local_copy.log'
 
-urlretrieve (URL_PATH, LOCAL_FILE )
+#urlretrieve (URL_PATH, LOCAL_FILE )
 
-print('File has downloaded.')
+#print('File has downloaded.')
 
 # File for question 2 solutions.
 # How many requests were made on each day? per week? per month?
+
+with open(local_copy.log, 'rb') as fh:
+    first = next(fh).decode()
+    fh.seek(-1024,2)
+    last = fh.readlines()[-1].decode()
+for line in fh:
+    pass
+last = line
+
+totaldays = 353
+# Calculate results based on total requests
+day = total/totaldays
+week = day/7
+month = week/4.35
+# Output results as averages request per period
+print ('Average Request per Day is', day)
+print ('Average Request per Week is', week)
+print ('Average Request per Month is', month)
