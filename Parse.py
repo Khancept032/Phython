2c932fbb7f08fe01eaf8a64090c13f4d447476cc
Abdul Khan <abdul_khan@tamu.edu>
2019-02-20 21:48:08 -0600

    Added log download code

--- Parse.py
+++ Parse.py
@@ -1,1 +1,13 @@
 # The master file of the programs will be placed in here once we have finished the programs for each question.
+
+print('Starting file download...')
+
+from urllib.request import urlretrieve
+
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+urlretrieve (URL_PATH, LOCAL_FILE )
+
+print('File has downloaded')
+
