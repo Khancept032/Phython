d7c3a37c36d8f988c3b6e2817faa6311c23b1ebb
Abdul Khan <abdul_khan@tamu.edu>
2019-02-20 21:32:09 -0600

    wrote code for not successful requests

--- q3.py
+++ q3.py
@@ -1,15 +1,37 @@
 # Download log file
-print('Starting file download...')
+#print('Starting file download...')
 
-from urllib.request import urlretrieve
+#from urllib.request import urlretrieve
 
-URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-LOCAL_FILE = 'local_copy.log'
+#URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+#LOCAL_FILE = 'local_copy.log'
 
-urlretrieve (URL_PATH, LOCAL_FILE )
+#urlretrieve (URL_PATH, LOCAL_FILE )
 
-print('File has downloaded.')
+#print('File has downloaded.')
 
 # File for question 3 solutions.
 
 # What percentage of the requests were not successful (any 4xx status code)?
+
+FILE_NAME = 'local_copy.log'  
+f = open('local_copy.log')
+lines = []
+count400 = 0
+
+for line in f:
+    line = line.strip()
+    lines.append(line)
+
+for a in range(len(lines)):
+    try:
+        results = lines[a].split()[8]
+    except:
+        continue
+    if results.startswith("4"):
+        count400 += 1
+        continue  
+   # else:
+       # continue  
+
+print ('Not successful requests 4xx:', (count400/len(lines)) * 100, 'percent') 
