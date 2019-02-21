85d67d759588c329ac265c4af1a0501be1573847
chrichoq <47570913+chrichoq@users.noreply.github.com>
2019-02-21 11:09:33 -0600

    Began Part 4

---
+++ part4.py
@@ -0,0 +1,16 @@
+# Part 4 splitting into 12 files
+
+import re
+    str = local_copy.log
+    a = re.findall("Jan",str)
+    b = re.findall("Feb".str)
+    c = re.findall("Mar".str)
+    d = re.findall("Apr".str)
+    e = re.findall("May".str)
+    f = re.findall("Jun".str)
+    g = re.findall("Jul".str)
+    h = re.findall("Aug".str)
+    i = re.findall("Sep".str)
+    j = re.findall("Oct".str)
+    k = re.findall("Nov".str)
+    l = re.findall("Dec".str)
