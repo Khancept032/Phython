2be91ff866138196cec26e24c73abfd51c2dcf1b
chrichoq <chrichoq@gmail.com>
2019-02-22 23:43:33 -0600

    Updated q2, correctly finds lines

--- q2.py
+++ q2.py
@@ -1,44 +1,17 @@
-# Download log file
-#print('Starting file download...')
+import datetime
+from datetime import date
+import re
 
-#from urllib.request import urlretrieve
-
-#URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-#LOCAL_FILE = 'local_copy.log'
-
-#urlretrieve (URL_PATH, LOCAL_FILE )
-
-#print('File has downloaded.')
-
-# File for question 2 solutions.
-# How many requests were made on each day? per week? per month?
 FILE_NAME = 'local_copy.log'  
 fh = open('local_copy.log')
 
-with open(local_copy.log, 'rb') as fh:
+with open('local_copy.log', 'rb') as fh:
     first = next(fh).decode()
-    fh.seek(-1024,2)
+
+    fh.seek(-1024, 2)
     last = fh.readlines()[-1].decode()
-for line in fh:
-    pass
-last = line
 
-import re
 
-af = re.search(r'(\d+/\d+/\d+/)',first)
-al = re.search(r'(\d+/\d+/\d+/)',last)
 
-from datetime import date
-af = date()
-al = date()
-totaldays = (al-af).days
-
-# Hard code totaldays = 353
-# Calculate results based on total requests
-day = total/totaldays
-week = day/7
-month = week/4.35
-# Output results as averages request per period
-print ('Average Request per Day is', day)
-print ('Average Request per Week is', week)
-print ('Average Request per Month is', month)
+print(first)
+print(last)
