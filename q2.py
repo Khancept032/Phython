61a97865bb3620302b015dc3bb32ea2c4399fa40
chrichoq <chrichoq@gmail.com>
2019-02-23 22:45:51 -0600

    Update q2.py

--- q2.py
+++ q2.py
@@ -7,11 +7,8 @@ fh = open('local_copy.log')
 
 with open('local_copy.log', 'rb') as fh:
     first = next(fh).decode()
-
     fh.seek(-1024, 2)
     last = fh.readlines()[-1].decode()
-
-
 
 print(first)
 print(last)
