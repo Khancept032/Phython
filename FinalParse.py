d91ca23bbd22ec2518f9a0c26ad212ddbbab29f6
chrichoq <chrichoq@gmail.com>
2019-02-26 23:44:36 -0600

    Updated final code

--- FinalParse.py
+++ FinalParse.py
@@ -50,7 +50,7 @@ print('Average Requests per Week:',int(round(total/finaltime*0.14285714)))
 print('Average Requests per Month:',int(round(total/finaltime*0.032854884)))
 #Question 3
 FILE_NAME = 'local_copy.log'  
-f = open('local_copy.log')
+f = open('local_copy.log',)
 lines = []
 count400 = 0
 for line in f:
@@ -237,6 +237,46 @@ elif pscount == min(files):
 elif htmlcount == min(files):
     print('html is least requested file:', min(files))
 
+print()
 print('Seperating into files by month...')
-#Insert FilesByMonth.py when complete
+f = open('local_copy.log', 'r')
+#Create seperate files by month
+jan_log = open('jan_log.log', 'w+')
+feb_log = open('feb_log.log', 'w+')
+mar_log = open('mar_log.log', 'w+')
+apr_log = open('apr_log.log', 'w+')
+may_log = open('may_log.log', 'w+')
+jun_log = open('jun_log.log', 'w+')
+jul_log = open('jul_log.log', 'w+')
+aug_log = open('aug_log.log', 'w+')
+sep_log = open('sep_log.log', 'w+')
+oct_log = open('oct_log.log', 'w+')
+nov_log = open('nov_log.log', 'w+')
+dec_log = open('dec_log.log', 'w+')
+
+for line in f:
+    if re.search((r'Jan'), line):
+        jan_log.write(line)
+    elif re.search((r'Feb'), line):
+        feb_log.write(line)
+    elif re.search((r'Mar'), line):
+        mar_log.write(line)
+    elif re.search((r'Apr'), line):
+        apr_log.write(line)
+    elif re.search((r'May'), line):
+        may_log.write(line)
+    elif re.search((r'Jun'), line):
+        jun_log.write(line)
+    elif re.search((r'Jul'), line):
+        jul_log.write(line)
+    elif re.search((r'Aug'), line):
+        aug_log.write(line)
+    elif re.search((r'Sep'), line):
+        sep_log.write(line)
+    elif re.search((r'Oct'), line):
+        oct_log.write(line)
+    elif re.search((r'Nov'), line):
+        nov_log.write(line)
+    elif re.search((r'Dec'), line):
+        dec_log.write(line)
 print('Files have downloaded.')
